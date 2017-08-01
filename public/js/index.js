1c7a93b9b7d7b9e56cbdcab73a896e6deffa73e8
rotcehxp <hector.diaz6@upr.edu>
2017-08-01 13:05:52 -0400

    Left side bar now gets updated with the user data. Right side bar added to send messages (backend is not done). Search bar now searches for another channels. Other changes.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,14 @@
+firebase.auth().onAuthStateChanged(function(user) {
+	if(user){
+		loggedInState(user);
+	}else{
+		loggedOutState();
+	}
+});
+
+
 /*Splash Screen Code*/
-var provider = new firebase.auth.FacebookAuthProvider();
+	var provider = new firebase.auth.FacebookAuthProvider();
 
     var fb_button = document.querySelector("#splash_fb_button");
     fb_button.addEventListener('click', function(e){
@@ -12,14 +21,22 @@ var provider = new firebase.auth.FacebookAuthProvider();
 	      		user_email = user.email,
 	     		user_profile_photo_url = user.photoURL,
 	     		user_id = user.uid;
+
 	 		
 	 		//Function to write user data
    			function writeUserData(wall_url, user_id, user_name, user_email, user_profile_photo_url) {
 				firebase.database().ref('users/' + user_id).set({
 	  				username: user_name,
 	   				email: user_email,
+	   				user_id: user_id,
 	   				profile_picture : user_profile_photo_url,
-	   				wall_url: wall_url
+	   				wall_url: wall_url,
+		     		followers_counter: 0,
+		     		following_counter: 0,
+		     		ice_received_counter: 0,
+		     		fire_received_counter: 0,
+		     		ice_given_counter: 0,
+		     		fire_given_counter: 0
 				});
 				firebase.database().ref('wall_url_collection/'+wall_url).set({
 					user_id: user_id
@@ -27,22 +44,19 @@ var provider = new firebase.auth.FacebookAuthProvider();
 			}
 
 			firebase.database().ref('/users/' + user_id).once('value', function(snapshot){
-				console.log("HERE A1");
 				//User login succesfully.
 				if(snapshot.val()!=null){
-					console.log("HERE A2");
-					$("#splash_request_wall_url").hide();
-					$("#splash_login").hide();
+					console.log(user);
+					loggedInState(user);
+
+				//User is not registered.
 				} else {
-					console.log("HERE A3");
-					//User is not registered.
-					$("#splash_login").hide();
+					registerState();
 					$("#splash_send_button").click(function(){
-						console.log("HERE A4");
 						var wall_url = $("#splash_input_create_wall_url").val();
-						//User do not entered a wall url.
+
+						//Wall url validation.
 						if(wall_url==''){
-							console.log("HERE A5");
 							$("#splash_error_message").replaceWith("<div class=splash_error_message id=splash_error_message>No has ingresado una dirección para tu muro.</div>");
 							$("#splash_error_message").fadeOut(5000);
 						} else if((/^[a-zA-Z0-9]*$/.test(wall_url) == false)||wall_url.indexOf(' ') >= 0){
@@ -55,18 +69,17 @@ var provider = new firebase.auth.FacebookAuthProvider();
 							$("#splash_error_message").replaceWith("<div class=splash_error_message id=splash_error_message>No puedes usar menos de 4 caracteres.</div>");
 							$("#splash_error_message").fadeOut(5000);
 						}else{
-							console.log("HERE A6");
 							firebase.database().ref('wall_url_collection/'+wall_url).once('value', function(snapshot) {
+
 								//Wall url is taken.
     							if(snapshot.val() != null){
-    								console.log("HERE A7");
     								$("#splash_error_message").replaceWith("<div class=splash_error_message id=splash_error_message>Esa dirección no está disponible. Intente otra.</div>");
 									$("#splash_error_message").fadeOut(5000);
+
+    							//Wall url is valid. User account is created.
     							} else if(snapshot.val() == null){
-    								console.log("HERE A8");
-    								//Wall url is valid. User account is created.
     								writeUserData(wall_url, user_id, user_name, user_email, user_profile_photo_url);
-    								$("#splash_request_wall_url").hide();		
+    								loggedInState(user);	
     							}
 							});
 						}
@@ -76,5 +89,85 @@ var provider = new firebase.auth.FacebookAuthProvider();
 		});
 	}, false);
 
-/*User wall code*/
 
+$("#input_search_wall").on('keyup', function (e) {
+    if (e.keyCode == 13) {
+    	console.log("HERE B1");
+    	var wall_url = $("#input_search_wall").val();
+        firebase.database().ref('wall_url_collection/'+wall_url).once('value', function(wall_url_reference) {
+        	console.log("HERE B2");
+			//Wall url is taken.
+    		if(wall_url_reference.val()!= null){
+    			console.log("HERE B3");
+    			var user_reference=wall_url_reference.child("user_id").val();
+    			firebase.database().ref('users/'+user_reference).once('value', function(user){
+    				console.log(user.val());
+    				fillWallWithOtherUserData(user.val());
+    			});
+    		} else{
+    			alert("El canal que intenta buscar no existe.");
+    		}
+		});
+    }
+});
+
+function loggedInState(user){
+    fillWallWithUserData(user);
+    $("#splash_request_wall_url").hide();
+	$("#splash_login").hide();
+}
+
+function loggedOutState(){
+	$("#splash_login").show();
+	$("#splash_request_wall_url").hide();
+}
+
+function registerState(){
+  	$("#splash_request_wall_url").show();
+	$("#splash_login").hide();
+}
+
+function fillWallWithUserData(user){
+    firebase.database().ref('users/'+user.uid).once('value', function(snapshot){
+    	var username_to_display = snapshot.child('username').val(),
+    	    followers_counter_to_display = snapshot.child('following_counter').val(),
+    		ice_received_counter_to_display = snapshot.child('ice_received_counter').val(),
+    		fire_received_counter_to_display = snapshot.child('fire_received_counter').val(),
+    		 user_profile_photo_url_to_display = snapshot.child('profile_picture').val();
+
+    	$("#username").replaceWith("<p id=username>"+username_to_display+"</p>");
+    	$("#wall_followers_counter_to_display").replaceWith("<p id=wall_followers_counter_to_display>"+followers_counter_to_display+" seguidores</p>");
+    	$("#wall_ice_received_counter_to_display").replaceWith("<p id=wall_ice_received_counter_to_display>"+ice_received_counter_to_display+" fríos</p>");
+    	$("#wall_fire_received_counter_to_display").replaceWith("<p id=wall_fire_received_counter_to_display>"+fire_received_counter_to_display+" fuegos</p>");
+    	$("#profile_picture_to_display").replaceWith("<img id=profile_picture_to_display src="+user_profile_photo_url_to_display+">");
+    });
+}
+
+function fillWallWithOtherUserData(user){
+	var user_loggedin = firebase.auth().currentUser;
+	if(user_loggedin.uid==user.user_id){
+		fillWallWithUserData(user_loggedin);
+	}else{
+		firebase.database().ref('users/'+user.user_id).once('value', function(snapshot){
+    	var username_to_display = snapshot.child('username').val(),
+    	    followers_counter_to_display = snapshot.child('following_counter').val(),
+    		ice_received_counter_to_display = snapshot.child('ice_received_counter').val(),
+    		fire_received_counter_to_display = snapshot.child('fire_received_counter').val(),
+    		 user_profile_photo_url_to_display = snapshot.child('profile_picture').val();
+
+    	$("#username").replaceWith("<p id=username>"+username_to_display+"</p>");
+    	$("#wall_followers_counter_to_display").replaceWith("<p id=wall_followers_counter_to_display>"+followers_counter_to_display+" seguidores</p>");
+    	$("#wall_ice_received_counter_to_display").replaceWith("<p id=wall_ice_received_counter_to_display>"+ice_received_counter_to_display+" fríos</p>");
+    	$("#wall_fire_received_counter_to_display").replaceWith("<p id=wall_fire_received_counter_to_display>"+fire_received_counter_to_display+" fuegos</p>");
+    	$("#profile_picture_to_display").replaceWith("<img id=profile_picture_to_display src="+user_profile_photo_url_to_display+">");
+    	});
+	}
+
+}
+
+//Check logout button
+$("#logout_button").click(function(){
+		console.log("HERE A12");
+		firebase.auth().signOut();
+		location.reload();
+});
